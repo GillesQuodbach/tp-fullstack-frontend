70ac839e095db676a27cfb9544ed0d53bad5db29
Alejandra Ramirez-Loyer <alejandra.ramirez-loyer@fms-ea.com>
2024-06-04 11:50:31 +0200

    patterns

--- src/app/model/customer.model.ts
+++ src/app/model/customer.model.ts
@@ -2,15 +2,15 @@ export class Customer {
     id : number;
     name : string;
     lastname : string;
-    adress : string;
+    address : string;
     phone : string;
     email : string;
 
-    constructor(id : number, name : string, lastname : string, adress : string, phone : string, email:string){
+    constructor(id : number, name : string, lastname : string, address : string, phone : string, email:string){
         this.id = id;
         this.name = name;
         this.lastname = lastname;
-        this.adress = adress;
+        this.address = address;
         this.phone = phone;
         this.email = email;
     }
